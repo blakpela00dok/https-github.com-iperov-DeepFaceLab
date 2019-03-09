a262a603ce76c63fe88ff0f5a2a1d860bcfbd028
wpydcr <wpydcr@sina.com>
2019-03-09 17:09:44 +0800

    fix keyword spell mistake in ModelBase

--- models/ModelBase.py
+++ models/ModelBase.py
@@ -223,7 +223,7 @@ class ModelBase(object):
         
     #overridable
     def get_converter(self):
-        raise NotImplementeError
+        raise NotImplementedError
         #return existing or your own converter which derived from base
      
     def get_target_epoch(self):
