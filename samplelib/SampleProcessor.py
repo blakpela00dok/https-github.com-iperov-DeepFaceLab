0f0df28b9f6d2ff92c7221b2263f54d5348b17df
Jeremy Hummel <jhummel@onica.com>
2019-08-10 01:05:48 -0700

    Formatting

--- samplelib/SampleProcessor.py
+++ samplelib/SampleProcessor.py
@@ -224,9 +224,9 @@ class SampleProcessor(object):
                     if ct_sample_bgr is None:
                         ct_sample_bgr = ct_sample.load_bgr()
 
-                    ct_sample_bgr_resized = cv2.resize(ct_sample_bgr, (resolution, resolution), cv2.INTER_LINEAR)
+                    # ct_sample_bgr_resized = cv2.resize(ct_sample_bgr, (resolution, resolution), cv2.INTER_LINEAR)
 
-                    img_bgr = imagelib.linear_color_transfer(img_bgr, ct_sample_bgr_resized)
+                    img_bgr = imagelib.reinhard_color_transfer(img_bgr, ct_sample_bgr[..., 0:3])
                     img_bgr = np.clip(img_bgr, 0.0, 1.0)
 
                 if normalize_std_dev:
