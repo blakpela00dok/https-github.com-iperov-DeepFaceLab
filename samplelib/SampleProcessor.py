85a3dd8ae3102aa461c720b032e4efd78c9798dd
Jeremy Hummel <jhummel@onica.com>
2019-08-13 01:26:19 -0700

    preserve paper

--- samplelib/SampleProcessor.py
+++ samplelib/SampleProcessor.py
@@ -235,7 +235,7 @@ class SampleProcessor(object):
                         img_bgr = imagelib.linear_color_transfer(img_bgr, ct_sample_bgr)
 
                     elif apply_ct == ColorTransferMode.RCT:
-                        img_bgr = imagelib.reinhard_color_transfer(img_bgr, ct_sample_bgr, clip=False)
+                        img_bgr = imagelib.reinhard_color_transfer(img_bgr, ct_sample_bgr, clip=False, preserve_paper=True,)
 
                     elif apply_ct == ColorTransferMode.RCT_MASKED:
                         if ct_sample_mask is None:
@@ -244,6 +244,7 @@ class SampleProcessor(object):
                         img_bgr = imagelib.reinhard_color_transfer(img_bgr,
                                                                    ct_sample_bgr,
                                                                    clip=False,
+                                                                   preserve_paper=True,
                                                                    target_mask=img_mask,
                                                                    source_mask=ct_sample_mask)
 
