844cfa48b0690a445b7c6758df308f08f930b2c9
cszdlt <cszdlt@qq.com>
2019-12-29 01:18:27 +0800

    还原 SAEHD 模型参数

--- models/Model_SAEHD/Model.py
+++ models/Model_SAEHD/Model.py
@@ -211,7 +211,7 @@ class SAEHDModel(ModelBase):
                                 x = Add()([x, x0])
                                 x = LeakyReLU(0.2)(x)
 
-                        return Conv2D(output_nc, kernel_size=1, padding='same', activation='sigmoid')(x)
+                        return Conv2D(output_nc, kernel_size=5, padding='same', activation='sigmoid')(x)
 
                     return func
 
@@ -325,7 +325,7 @@ class SAEHDModel(ModelBase):
                                 x = Add()([x, x0])
                                 x = LeakyReLU(0.2)(x)
 
-                        return Conv2D(output_nc, kernel_size=1, padding='same', activation='sigmoid')(x)
+                        return Conv2D(output_nc, kernel_size=5, padding='same', activation='sigmoid')(x)
 
                     return func
 
