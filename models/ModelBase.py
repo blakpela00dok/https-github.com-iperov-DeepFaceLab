8b4e2347214f755f1b87f551c8857da915d8d974
leno3003 <cassanoenrico53@gmail.com>
2022-03-30 18:31:45 +0200

    added sumbmodules

--- models/ModelBase.py
+++ models/ModelBase.py
@@ -295,6 +295,9 @@ class ModelBase(object):
                 self.choose_preview_history = io.input_bool("Randomly choose new image for preview history", False, help_message="Preview image history will stay stuck with old faces if you reuse the same model on different celebs. Choose no unless you are changing src/dst to a new person")
 
     def ask_target_iter(self, default_value=0):
+        print("Current directory")
+        cmd = "pwd"
+        os.system(cmd)
         answer_filename = 'workspace/interact/interact_dict.pkl'
         if os.path.exists(answer_filename):
             with open(answer_filename, 'rb') as file:
