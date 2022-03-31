c66db83b17c5730c52dbf9b12c1dc20f56423674
leno3003 <cassanoenrico53@gmail.com>
2022-03-31 11:27:31 +0200

    added sumbmodules

--- core/interact/interact.py
+++ core/interact/interact.py
@@ -228,7 +228,7 @@ class InteractBase(object):
             return self.default_answers[answer_key]
         return None
 
-    def input(self, s, answer_key):
+    def input(self, s, answer_key=None):
         ans = self.get_default_answer(answer_key)
         if ans is not None:
             return ans
